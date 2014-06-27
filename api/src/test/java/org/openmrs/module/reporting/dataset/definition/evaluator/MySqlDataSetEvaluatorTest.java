62f17d1d5209742c798c6ac9c232d6db9dfdc957
mseaton <mseaton@pih.org>
2014-06-27 00:05:10 -0400

    REPORT-641 - SqlDataSetDefinitions do not properly handle List parameters.
    Update SqlDataSetEvaluator to use the HqlQueryBuilder class (and to execute SQL queries via a hibernate SQL query) rather than to use our own stored procedure execution.

---
+++ api/src/test/java/org/openmrs/module/reporting/dataset/definition/evaluator/MySqlDataSetEvaluatorTest.java
@@ -0,0 +1,55 @@
+package org.openmrs.module.reporting.dataset.definition.evaluator;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openmrs.Location;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.service.DataSetDefinitionService;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+@Ignore
+public class MySqlDataSetEvaluatorTest extends BaseModuleContextSensitiveTest {
+
+	@Override
+	public Boolean useInMemoryDatabase() {
+		return false;
+	}
+
+	@Before
+	public void setup() throws Exception {
+		authenticate();
+	}
+
+	/**
+	 * @return MS Note: use port 3306 as standard, 5538 for sandbox 5.5 mysql environment
+	 */
+	@Override
+	public Properties getRuntimeProperties() {
+		Properties p = super.getRuntimeProperties();
+		p.setProperty("connection.url", "jdbc:mysql://localhost:3306/openmrs?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8");
+		return p;
+	}
+
+	@Test
+	public void evaluate_shouldHandleMetadataListParameters() throws Exception {
+		SqlDataSetDefinition dataSetDefinition = new SqlDataSetDefinition();
+		String query = "select encounter_id, encounter_datetime, location_id from encounter where location_id in (:locations)";
+		dataSetDefinition.setSqlQuery(query);
+
+		List<Location> locationList = new ArrayList<Location>();
+		locationList.add(Context.getLocationService().getLocation(1));
+		locationList.add(Context.getLocationService().getLocation(3));
+
+		EvaluationContext context = new EvaluationContext();
+		context.addParameterValue("locations", locationList);
+
+		Context.getService(DataSetDefinitionService.class).evaluate(dataSetDefinition, context);
+	}
+}
